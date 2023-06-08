b728529197a722d5457806d9b249138b49954db0
ACM-Anik <anikmojumder5252@gmail.com>
2023-06-09 05:57:47 +0600

    patch for Instructor done

--- index.js
+++ index.js
@@ -47,7 +47,19 @@ async function run() {
             res.send(result);
         })
 
-        app.patch('users/admin/:id', async (req, res) => {
+        app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    role: 'Instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = {_id: new ObjectId(id)};
             const updateDoc = {
