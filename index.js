3b33aa869be8d52727a1486414fa724b859b81cf
ACM-Anik <anikmojumder5252@gmail.com>
2023-06-09 06:48:16 +0600

    JWT token setup done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,24 @@ const port = process.env.PORT || 5000;
 // Middleware:-
 app.use(cors());
 app.use(express.json());
+
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'Unauthorized access' });
+    }
+    // (Bearer) (token)
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'Unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+} 
 
 
 
@@ -32,6 +51,13 @@ async function run() {
         const usersCollection = client.db('danceRevolutions').collection('users');
         const classesCollection = client.db('danceRevolutions').collection('classes');
         const selectedClassesCollection = client.db('danceRevolutions').collection('selectedClasses');
+
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+            res.send({token});
+        })
 
 
         // Slider Categories API:
